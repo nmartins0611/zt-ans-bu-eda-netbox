c345d573ec8168e19af39529d5f406df00f5af31
Nuno Martins <nmartins@redhat.com>
2025-10-14 12:17:57 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -7,141 +7,33 @@ nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 
 nmcli connection up enp2s0
 echo "192.168.1.10 control.lab control" >> /etc/hosts
 
-# # Setup rhel user
-# cp -a /root/.ssh/* /home/rhel/.ssh/.
-# chown -R rhel:rhel /home/rhel/.ssh
-# mkdir -p /home/rhel/lab_exercises/1.Terraform_Basics
-# mkdir -p /home/rhel/lab_exercises/2.Terraform_Ansible
-# mkdir -p /home/rhel/lab_exercises/3.Terraform_Provider
-# mkdir -p /home/rhel/lab_exercises/4.Terraform_AAP_Provider
-# mkdir -p /home/rhel/terraform-ee
-# mkdir /tmp/terraform_lab/
-# mkdir /tmp/terraform-ansible
-# mkdir /tmp/terraform-aap-provider
-# mkdir -p /home/rhel/.terraform.d/plugin-cache
-# #
-# #
-# #chown rhel:rhel /home/rhel/.terraformrc
-# chown -R rhel:rhel /home/rhel/lab_exercises/
-# chown rhel:rhel /home/rhel/.terraform.d/plugin-cache
-# chmod -R 777 /home/rhel/lab_exercises/
-# #
-# firewall-cmd --permanent --add-port=8043/tcp
-# firewall-cmd --reload
-# #
-# yum install -y unzip
-# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-# unzip -qq awscliv2.zip
-# sudo ./aws/install
-# chown -R rhel:rhel /home/rhel/lab_exercises
-# chmod -R 777 /home/rhel/lab_exercises
-# #
-# yum install -y dnf
-# dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-# yum install terraform -y
-# #
-# #
-# tee /home/rhel/lab_exercises/4.Terraform_AAP_Provider/main.tf << EOF
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "6.2.0"
-#     }
-# ####### UNCOMMENT the lines BELOW #######
-# #    aap = {
-# #      source = "ansible/aap"
-# #    }
-#   }
-# }
-# #
-# provider "aws" {
-#   region = "us-east-1"
-# }
-# #
-# resource "aws_instance" "tf-instance-aap-provider" {
-#   ami           = "ami-0005e0cfe09cc9050"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "tf-instance-aap-provider"
-#   }
-# }
-# ####### UNCOMMENT the lines BELOW #######
-# #provider "aap" {
-# #  host     = "https://controller"
-# #  username = "admin"
-# #  password = "ansible123!"
-# #  insecure_skip_verify = true
-# #}
-# ####### UNCOMMENT the lines BELOW #######
-# #resource "aap_host" "tf-instance-aap-provider" {
-# #  inventory_id = 2
-# #  name = "aws_instance_tf"
-# #  description = "An EC2 instance created by Terraform"
-# #  variables = jsonencode(aws_instance.tf-instance-aap-provider)
-# #}
-# #
-# EOF
-# #
-# chown rhel:rhel /home/rhel/lab_exercises/4.Terraform_AAP_Provider/main.tf
-# #
-# #
+########
 
 
-# # Create directory if it doesn't exist
-# mkdir -p /home/rhel/.aws
+RHEL_SSH_DIR="/home/rhel/.ssh"
+RHEL_PRIVATE_KEY="$RHEL_SSH_DIR/id_rsa"
+RHEL_PUBLIC_KEY="$RHEL_SSH_DIR/id_rsa.pub"
 
-# # Create the credentials file
-# cat > /home/rhel/.aws/credentials << EOF
-# [default]
-# aws_access_key_id = $AWS_ACCESS_KEY_ID
-# aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
-# EOF
+if [ -f "$RHEL_PRIVATE_KEY" ]; then
+    echo "SSH key already exists for rhel user: $RHEL_PRIVATE_KEY"
+else
+    echo "Creating SSH key for rhel user..."
+    sudo -u rhel mkdir -p /home/rhel/.ssh
+    sudo -u rhel chmod 700 /home/rhel/.ssh
+    sudo -u rhel ssh-keygen -t rsa -b 4096 -C "rhel@$(hostname)" -f /home/rhel/.ssh/id_rsa -N "" -q
+    sudo -u rhel chmod 600 /home/rhel/.ssh/id_rsa*
+    
+    if [ -f "$RHEL_PRIVATE_KEY" ]; then
+        echo "SSH key created successfully for rhel user"
+    else
+        echo "Error: Failed to create SSH key for rhel user"
+    fi
+fi
 
-# # Set proper ownership and permissions
-# chown rhel:rhel /home/rhel/.aws/credentials
-# chmod 600 /home/rhel/aws/credentials
 
-# cat > /home/rhel/aws/config << EOF
-# [default]
-# region = $AWS_DEFAULT_REGION
-# EOF
 
-# # Set proper ownership and permissions
-# chown rhel:rhel /home/rhel/aws/config
-# chmod 600 /home/rhel/aws/config
 
-# #
-# #Create the DEFAULT AWS VPC
-# su - rhel -c "aws ec2 create-default-vpc --region $AWS_DEFAULT_REGION"
-# #
-# #
-# #Create the S3 bucket for the users of this AAP / Terraform lab
-# # Variables
-# BUCKET_PREFIX="aap-tf-bucket"  # Change this to your desired bucket prefix
-# RANDOM_ID=$(uuidgen | tr '[:upper:]' '[:lower:]')  # Generate a random UUID and convert to lowercase
-# BUCKET_NAME="${BUCKET_PREFIX}-${RANDOM_ID}"
-# AWS_REGION="$AWS_DEFAULT_REGION"  # Change this to your desired AWS region
-# #
-# #
-# # Create the S3 STORAGE BUCKET NEEDED BY THE AAP 2.X CHALLENGE
-# echo "Creating S3 bucket: $BUCKET_NAME in region $AWS_DEFAULT_REGION"
-# su - rhel -c "aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION"
-# #
-# # ## ansible home
-# # mkdir /home/$USER/ansible
-# # ## ansible-files dir
-# # mkdir /home/$USER/ansible-files
 
-# # ## ansible.cfg
-# # echo "[defaults]" > /home/$USER/.ansible.cfg
-# # echo "inventory = /home/$USER/ansible-files/hosts" >> /home/$USER/.ansible.cfg
-# # echo "host_key_checking = False" >> /home/$USER/.ansible.cfg
-
-# # ## chown and chmod all files in rhel user home
-# # chown -R rhel:rhel /home/$USER/ansible
-# # chmod 777 /home/$USER/ansible
-# # chown -R rhel:rhel /home/$USER/ansible-files
 
 ########
 ## install python3 libraries needed for the Cloud Report
@@ -159,128 +51,161 @@ tee /tmp/setup.yml << EOF
     - ansible.controller
 
   vars:
-    aws_access_key: "{{ lookup('env', 'AWS_ACCESS_KEY_ID') | default('AWS_ACCESS_KEY_ID_NOT_FOUND', true) }}"
-    aws_secret_key: "{{ lookup('env', 'AWS_SECRET_ACCESS_KEY') | default('AWS_SECRET_ACCESS_KEY_NOT_FOUND', true) }}"
-    aws_default_region: "{{ lookup('env', 'AWS_DEFAULT_REGION') | default('AWS_DEFAULT_REGION_NOT_FOUND', true) }}"
-    quay_username: "{{ lookup('env', 'QUAY_USERNAME') | default('QUAY_USERNAME_NOT_FOUND', true) }}"
-    quay_password: "{{ lookup('env', 'QUAY_PASSWORD') | default('QUAY_PASSWORD_NOT_FOUND', true) }}"
+    controller_host: localhost
+    GUID: "{{ lookup('ansible.builtin.env', 'GUID') }}"
+    DOMAIN: "{{ lookup('ansible.builtin.env', 'DOMAIN') }}"
+    SANDBOX_ID: "{{ GUID }}.{{ DOMAIN }}"
+    controller_host: "https://localhost"
+    controller_username: admin
+    controller_password: ansible123!
+    inventory_name: netbox-inventory
+    credentials_name: cat8000v-credential
+    NETBOX_API_VAR: "{{ '{{' }} NETBOX_API {{ '}}' }}"
+    NETBOX_TOKEN_VAR: "{{ '{{' }} NETBOX_TOKEN {{ '}}' }}"
 
-  tasks:
-    - name: Add AWS credential
+  tasks: cat8000v-credential
+    - name: Add network machine credential
       ansible.controller.credential:
-        name: 'AWS Credential'
-        organization: Default
-        credential_type: "Amazon Web Services"
+        name: "cat8000v-credential"
+        organization: "Default"
+        credential_type: Machine
         controller_host: "https://localhost"
         controller_username: admin
         controller_password: ansible123!
-        validate_certs: false
+        validate_certs: fals
         inputs:
-          username: "{{ aws_access_key }}"
-          password: "{{ aws_secret_key }}"
+          username: "admin"
+          password: "ansible123!"
 
-    # - name: Remove Demo Inventory
-    #   ansible.controller.inventory:
-    #     controller_host: "https://localhost"
-    #     controller_username: admin
-    #     controller_password: ansible123!
-    #     validate_certs: false
-    #     name: "Demo Inventory"
-    #     organization: Default
-    #     state: absent
-
-    # - name: Ensure inventory exists
-    #   ansible.controller.inventory:
-    #     controller_host: "https://localhost"
-    #     controller_username: admin
-    #     controller_password: ansible123!
-    #     validate_certs: false
-    #     name: "AWS Inventory example"
-    #     organization: Default
-    #     state: present
-    #   register: aws_inventory_result
-
-    # - name: Ensure AWS EC2 inventory source exists
-    #   ansible.controller.inventory_source:
-    #     controller_host: "https://localhost"
-    #     controller_username: admin
-    #     controller_password: ansible123!
-    #     validate_certs: false
-    #     name: "AWS EC2 Instances Source"
-    #     inventory: "AWS Inventory example"
-    #     source: ec2
-    #     credential: "AWS Credential"
-    #     source_vars:
-    #       regions: ["{{ aws_default_region }}"]
-    #     overwrite: true
-    #     overwrite_vars: true
-    #     update_on_launch: true
-    #     update_cache_timeout: 300
-    #     state: present
-    #   register: aws_inventory_source_result
-
-    - name: Add a Container Registry Credential to automation controller
-      ansible.controller.credential:
-        name: Quay Registry Credential
-        description: Creds to be able to access Quay
-        organization: "Default"
+    - name: (EXECUTION) Create credential type for NetBox
+      ansible.controller.credential_type:
+        name: netbox-setup-api
+        description: Credentials type for NetBox
+        kind: cloud
+        inputs: 
+          fields:
+            - id: NETBOX_API
+              type: string
+              label: NetBox Host URL
+            - id: NETBOX_TOKEN
+              type: string
+              label: NetBox API Token
+              secret: true
+          required:
+            - NETBOX_API
+            - NETBOX_TOKEN
+        injectors:
+          env:
+            NETBOX_API: "{{ NETBOX_API_VAR }}"
+            NETBOX_TOKEN: "{{ NETBOX_TOKEN_VAR }}"
         state: present
-        credential_type: "Container Registry"
         controller_username: admin
         controller_password: ansible123!
-        controller_host: "https://localhost"
         validate_certs: false
+        
+    - name: (EXECUTION) Create credentials for the netbox-setup
+      ansible.controller.credential:
+        validate_certs: false
+        controller_username: admin
+        controller_password: ansible123!
+        name: netbox-setup-creds
+        credential_type: netbox-setup-api
+        organization: Default
         inputs:
-          username: "{{ quay_username }}"
-          password: "{{ quay_password }}"
-          host: "quay.io"
-      register: controller_try
-      retries: 10
-      until: controller_try is not failed
+          NETBOX_API: "http://netbox:8000"
+          NETBOX_TOKEN: "0123456789abcdef0123456789abcdef01234567"
 
-    - name: Add EE to the controller instance
+    - name: (EXECUTION) Create new execution environment
       ansible.controller.execution_environment:
-        name: "Terraform Execution Environment"
-        image: quay.io/acme_corp/terraform_ee
-        credential: Quay Registry Credential
+        validate_certs: false
         controller_username: admin
         controller_password: ansible123!
-        controller_host: "https://localhost"
+        name: network-ee
+        image: ghcr.io/ansible-network/autocon-ee:latest
+        pull: missing
+        
+    - name: (EXECUTION) Create new execution environment
+      ansible.controller.execution_environment:
         validate_certs: false
+        controller_username: admin
+        controller_password: ansible123!
+        name: netbox-ee
+        image:  quay.io/acme_corp/network-netbox-eda-ee:latest
+        pull: missing
 
-    - name: Add project
+    - name: (EXECUTION) Create new Project from git
       ansible.controller.project:
-        name: "Terraform Demos Project"
-        description: "This is from github.com/ansible-cloud"
-        organization: "Default"
+        name: "netbox-setup-project"
+        organization: Default
         state: present
         scm_type: git
-        scm_url: http://gitea:3000/student/terraform-aap.git
-        default_environment: "Terraform Execution Environment"
+        scm_url: https://github.com/ansible-tmm/ansible-netbox-setup.git
+        validate_certs: false
         controller_username: admin
         controller_password: ansible123!
-        controller_host: "https://localhost"
-        validate_certs: false
 
-    - name: Delete native job template
-      ansible.controller.job_template:
-        name: "Demo Job Template"
-        state: "absent"
-        controller_username: admin
-        controller_password: ansible123!
-        controller_host: "https://localhost"
-        validate_certs: false
-
-    - name: Add a TERRAFORM INVENTORY
+    - name: (EXECUTION) Create an inventory in automation controller
       ansible.controller.inventory:
-        name: "Terraform Inventory"
-        description: "Our Terraform Inventory"
-        organization: "Default"
-        state: present
+        name: netbox-setup-inventory
+        organization: Default
+        validate_certs: false
         controller_username: admin
         controller_password: ansible123!
-        controller_host: "https://localhost"
+
+    - name: (EXECUTION) Create a new Job Template
+      ansible.controller.job_template:
+        name: "netbox-config-playbook"
+        job_type: "run"
+        organization: "Default"
+        state: "present"
+        inventory: "netbox-setup-inventory"
+        become_enabled: True
+        playbook: "netbox_setup.yml"
+        project: "netbox-setup-project"
+        credential: "netbox-setup-creds"
+        execution_environment: "netbox-ee"
         validate_certs: false
+        controller_username: admin
+        controller_password: ansible123!        
+
+    # - name: Debug SANDBOX_ID
+    #   ansible.builtin.debug:
+    #     msg: "https://control.{{ SANDBOX_ID }}/api/controller/"
+    #   vars:
+    #     SANDBOX_ID: "{{ lookup('env', '_SANDBOX_ID') | default('SANDBOX_ID_NOT_FOUND', true) }}"
+    
+    - name: (DECISIONS) Create an AAP Credential
+      ansible.eda.credential:
+        name: "AAP"
+        description: "To execute jobs from EDA"
+        inputs:
+   #       host: "https://control.{{ SANDBOX_ID }}.instruqt.io/api/controller/"
+           host: "https://control.ansible.workshop/api/controller/"
+          username: "admin"
+          password: "ansible123!"
+        credential_type_name: "Red Hat Ansible Automation Platform"
+   #     controller_host: "https://control.{{ SANDBOX_ID }}.instruqt.io"
+        controller_host: "https://control.ansible.workshop/api/controller/"
+        controller_username: admin
+        controller_password: ansible123!
+        validate_certs: false
+        organization_name: Default
+      # vars:
+      #   SANDBOX_ID: "{{ lookup('env', '_SANDBOX_ID') | default('SANDBOX_ID_NOT_FOUND', true) }}"
+    
+    - name: (DECISIONS) Create a new DE
+      ansible.eda.decision_environment:
+   #     controller_host: "https://control.{{ SANDBOX_ID }}.instruqt.io"
+        controller_host: "https://control.ansible.workshop/api/controller/"
+        controller_username: admin
+        controller_password: ansible123!
+        validate_certs: false
+        organization_name: Default
+        name: "NetOps Decision Environment"
+        description: "Decision Environment for NetOps workshop"
+        image_url: "ghcr.io/ansible-network/autocon-de:latest"
+      # vars:
+      #   SANDBOX_ID: "{{ lookup('env', '_SANDBOX_ID') | default('SANDBOX_ID_NOT_FOUND', true) }}"
       
 EOF
 export ANSIBLE_LOCALHOST_WARNING=False
