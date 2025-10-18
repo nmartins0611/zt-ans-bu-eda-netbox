94febdecef86658e9ab0dd18d20097298be2be27
Nuno Martins <nmartins@redhat.com>
2025-10-18 17:14:19 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -6,6 +6,8 @@ systemctl disable systemd-tmpfiles-setup.service
 nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
 nmcli connection up enp2s0
 echo "192.168.1.10 control.lab control" >> /etc/hosts
+echo "192.168.1.11 netbox.lab netbox" >> /etc/hosts
+echo "192.168.1.12 devtools.lab devtools" >> /etc/hosts
 
 ########
 
