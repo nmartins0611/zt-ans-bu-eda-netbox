408d9cdf84941611b474818da266d1a583497bef
Nuno Martins <nmartins@redhat.com>
2025-10-27 19:14:58 +0200

    Update setup-netbox.sh

--- runtime-automation/module-07/setup-netbox.sh
+++ runtime-automation/module-07/setup-netbox.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+echo "Starting module called module-07" >> /tmp/progress.log
 
 docker compose --project-directory=/tmp/netbox-docker stop
 docker compose --project-directory=/tmp/netbox-docker up -d netbox netbox-worker
